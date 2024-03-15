f103dca1b5bcba9547273822cb5e18337724b3b4
walid-damou <walid.damou.2015@gmail.com>
2024-03-15 09:14:01 +0100

    creation_deploiFireBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"dicedelice-8d551","appId":"1:1024789472025:web:57ae4127c324f26a011b88","storageBucket":"dicedelice-8d551.appspot.com","apiKey":"AIzaSyA99iISR9ZyXbaw-293xIoPb-GboczMUJE","authDomain":"dicedelice-8d551.firebaseapp.com","messagingSenderId":"1024789472025","measurementId":"G-6EYYKFSJNE"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
