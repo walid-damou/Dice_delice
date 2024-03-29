1b62e90f23bbb0cfa6f0164493cb8dd884556ab3
Ayoub <ayoub.idrissi.kaitouni82@gmail.com>
2024-03-29 09:42:31 +0100

    Google Authentication2

--- src/app/components/login/form/form.component.ts
+++ src/app/components/login/form/form.component.ts
@@ -23,11 +23,8 @@ export class FormComponent implements OnInit {
   }
   signInWithGoogle() {
     this.authService.signInWithGoogle().then(() => {
-      // You can add any additional logic here that should run after successful sign-in
-      // For example, navigating to a different page or updating the UI
     }).catch(error => {
       console.error("Error during Google sign-in:", error);
-      // Handle any sign-in errors here, such as displaying a notification to the user
     });
   }
 }
