193aa50609e80081cb8ef974af3229edbbb653d3
yasmineslisli <yasmine.essalmani@uit.ac.ma>
2024-04-18 00:58:04 +0200

    myreservation done

--- src/app/service/myReservations/my-reservation.service.ts
+++ src/app/service/myReservations/my-reservation.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyReservationService {
 
-  constructor(private db:AngularFirestore) { }
+  private gameNameMapping: { [id: string]: string } = {
+    '6EOxjXgpjZiwcTTDnR1t': 'Uno',
+    'JQ9XoSpKpmSTzB5yqEOC': 'Scrabble',
+    'LeLkcyQgDws5RK0JsO3s': 'Monopoly',
+    'grvmhers8KRhY3t5U9rM': 'Jenga',
+    'q6Z8hd8XIThhOSpGEXHh': 'Ludo'
+  };
+
+  constructor(private db: AngularFirestore) { }
+
   getReservations(): Observable<any[]> {
-    const reservations = this.db.collection('Reservations').valueChanges();
-    return reservations;
+    return this.db.collection('Reservations').valueChanges().pipe(
+      map((reservations: any[]) => {
+        return reservations.map(reservation => {
+          const gameId = reservation.id_game;
+          const gameName = this.gameNameMapping[gameId] || 'Unknown Game';
+          return { ...reservation, game_name: gameName };
+        });
+      })
+    );
   }
 }
